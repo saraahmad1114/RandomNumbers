a866e62e67a7f4c49a69243cfb76dc0c4115a68c
Sara <saraahmad1114@yahoo.com>
2016-12-19 22:38:51 -0500

    second method

--- RandomNumberFacts/RandomNumberFacts/NumbersAPIClient.swift
+++ RandomNumberFacts/RandomNumberFacts/NumbersAPIClient.swift
@@ -37,5 +37,33 @@ class NumbersAPIClient {
         }
         task.resume()
     }
+    
+    class func getNumberTrivia ( number: Int, completion:@escaping(String)-> ()){
+    
+        var storedString = ""
+        
+        let urlString = "http://numbersapi.com/\(number)/trivia"
+        
+        let url = URL(string: urlString)
+        
+        guard let unwrappedURL = url else {print("url did not unwrap"); return}
+        
+        let request = URLRequest(url: unwrappedURL)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let unwrappedData = data else {print("Data did not unwrap"); return}
+            
+            let responseString = try? JSONSerialization.jsonObject(with: unwrappedData, options: []) as! String
+            
+            guard let unwrappedResponseString = responseString else {print("response string did not unwrap"); return}
+            
+            storedString = unwrappedResponseString
+            
+            completion(storedString)
+        }
+        task.resume()
+        
+    }
 
 }
